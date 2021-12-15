fe79df68f0c8c2dfe238673af3b1297eb061c496
emmanuel <gbengacode>
2021-12-15 20:29:50 +0100

    Modified form.js

--- src/events/form.js
+++ src/events/form.js
@@ -1,15 +1,15 @@
 import { createUser } from '../apis/index.js';
 
+const btnSubmit = document.querySelector('.submit');
+const user = document.querySelector('.user');
+const score = document.querySelector('.score');
 export default () => {
-  const btnSubmit = document.querySelector('.submit');
-  const user = document.querySelector('.user');
-  const score = document.querySelector('.score');
   btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (user.value.trim() === '') return;
+    if (score.value.trim() === '') return;
     btnSubmit.disabled = true;
     btnSubmit.textContent = 'Processing...';
-    e.preventDefault();
-    if (user.value.trim === '') return;
-    if (score.value.trim === '') return;
     createUser(user, score, btnSubmit);
   });
 };
